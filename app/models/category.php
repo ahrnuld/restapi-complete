980bd1454cc6e3ae3190ee05232d8ba89e61b78e
Mark-Inholland <mark.dehaan@inholland.nl>
2022-02-27 16:14:24 +0100

    Added categories

---
+++ app/models/category.php
@@ -0,0 +1,11 @@
+<?php
+namespace Models;
+
+class Category {
+
+    public int $id;
+    public string $name;
+  
+}
+
+?>
--- app/public/index.php
+++ app/public/index.php
@@ -12,12 +12,19 @@ $router = new \Bramus\Router\Router();
 
 $router->setNamespace('Controllers');
 
-// routes for the articles endpoint
+// routes for the products endpoint
 $router->get('/products', 'ProductController@getAll');
 $router->get('/products/(\d+)', 'ProductController@getOne');
 $router->post('/products', 'ProductController@create');
 $router->put('/products/(\d+)', 'ProductController@update');
 $router->delete('/products/(\d+)', 'ProductController@delete');
+
+// routes for the categories endpoint
+$router->get('/categories', 'CategoryController@getAll');
+$router->get('/categories/(\d+)', 'CategoryController@getOne');
+$router->post('/categories', 'CategoryController@create');
+$router->put('/categories/(\d+)', 'CategoryController@update');
+$router->delete('/categories/(\d+)', 'CategoryController@delete');
 
 // Run it!
 $router->run();
