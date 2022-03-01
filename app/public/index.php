a8a515d08d84bd29adff34e9fd5a869912a1f22c
Mark-Inholland <mark.dehaan@inholland.nl>
2022-03-01 21:24:52 +0100

    Allowed all method headers

--- app/public/index.php
+++ app/public/index.php
@@ -1,6 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods: *");
 
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
