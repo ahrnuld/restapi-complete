e51323ab6b38b8479bb133a1331a413fa240fd4e
Mark-Inholland <mark.dehaan@inholland.nl>
2022-03-08 20:50:21 +0100

    Added a user table to the .sql file

--- sql/developmentdb.sql
+++ sql/developmentdb.sql
@@ -24,8 +24,36 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Tabelstructuur voor tabel `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+INSERT INTO `user` (`id`, `username`, `password`, `email`) VALUES
+(1, 'username', '$2y$10$DQlV0u9mFmtOWsOdxXX9H.4kgzEB3E8o97s.S.Pdy4klUAdBvtVh.', 'username@password.com');
+
+
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+-- --------------------------------------------------------
+
+--
 -- Tabelstructuur voor tabel `category`
 --
+
+
 
 CREATE TABLE `category` (
   `id` int(11) NOT NULL,
